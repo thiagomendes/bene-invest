72af3b3f2f640c678dd55d68bad288dd3ef5b3d8
Thiago Mendes <tmendes@localhost.localdomain>
2018-12-01 16:58:13 -0200

    first commit

---
+++ src/test/java/br/com/beneinvest/beneinvest/BeneInvestApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.beneinvest.beneinvest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BeneInvestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
